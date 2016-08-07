f8db9c2fdbfb547be8d37dfeaf37bf19592bcf6e
kingsofcoding <simplecooldude1@gmail.com>
2016-08-07 05:21:00 +0000

    remove the & symbol

--- js/scripts.js
+++ js/scripts.js
@@ -10,7 +10,7 @@ document.getElementById("submit").addEventListener("click", function() {
 var totalLines = 0;
 
 function calc(userName) {
-fetch('https://api.github.com/users/' + userName + '/repos?&per_page=500&client_id=5c198d87bf1396233db3&client_secret=6a17404d965bfe175c7675d605147def3b1032d7')
+fetch('https://api.github.com/users/' + userName + '/repos?per_page=500&client_id=5c198d87bf1396233db3&client_secret=6a17404d965bfe175c7675d605147def3b1032d7')
   .then(res => {
     return res.json()
   })
@@ -18,7 +18,7 @@ fetch('https://api.github.com/users/' + userName + '/repos?&per_page=500&client_
     for (var i = 0; i < data.length; i++) {
       if(data[i].fork !== true) {
         console.log(data.length)
-      fetch('https://api.github.com/repos/' + data[i].full_name + '/stats/contributors?&per_page=500&client_id=5c198d87bf1396233db3&client_secret=6a17404d965bfe175c7675d605147def3b1032d7')
+      fetch('https://api.github.com/repos/' + data[i].full_name + '/stats/contributors?per_page=500&client_id=5c198d87bf1396233db3&client_secret=6a17404d965bfe175c7675d605147def3b1032d7')
         .then(response => response.json())
         .then(contributors => contributors
           .map(contributor => contributor.weeks
