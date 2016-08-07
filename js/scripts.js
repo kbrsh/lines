2836197557f733f9a9c6194629a074f92f3ed851
kingsofcoding <simplecooldude1@gmail.com>
2016-08-07 05:20:10 +0000

    all repos

--- js/scripts.js
+++ js/scripts.js
@@ -10,14 +10,15 @@ document.getElementById("submit").addEventListener("click", function() {
 var totalLines = 0;
 
 function calc(userName) {
-fetch('https://api.github.com/users/' + userName + '/repos?client_id=5c198d87bf1396233db3&client_secret=6a17404d965bfe175c7675d605147def3b1032d7')
+fetch('https://api.github.com/users/' + userName + '/repos?&per_page=500&client_id=5c198d87bf1396233db3&client_secret=6a17404d965bfe175c7675d605147def3b1032d7')
   .then(res => {
     return res.json()
   })
   .then(data => {
     for (var i = 0; i < data.length; i++) {
       if(data[i].fork !== true) {
-      fetch('https://api.github.com/repos/' + data[i].full_name + '/stats/contributors?client_id=5c198d87bf1396233db3&client_secret=6a17404d965bfe175c7675d605147def3b1032d7')
+        console.log(data.length)
+      fetch('https://api.github.com/repos/' + data[i].full_name + '/stats/contributors?&per_page=500&client_id=5c198d87bf1396233db3&client_secret=6a17404d965bfe175c7675d605147def3b1032d7')
         .then(response => response.json())
         .then(contributors => contributors
           .map(contributor => contributor.weeks
